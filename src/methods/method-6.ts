124994d4d0ec9cc98a5b63cbd2eaba91710d23b7
ZhaoCe <17631807110@163.com>
2022-12-12 12:03:09 +0800

    特殊类型 demo

--- src/methods/method-6.ts
+++ src/methods/method-6.ts
@@ -46,3 +46,58 @@ type IsTuple<T> = T extends [...params: infer Eles]
     : false;
 type IsTupleResult = IsTuple<[1, 2, 3]>;
 type IsTupleResult2 = IsTuple<number[]>;
+
+// UnionToIntersection
+type UnionToIntersection<U> = (
+    U extends U ? (x: U) => unknown : never
+) extends (x: infer R) => unknown
+    ? R
+    : never;
+type UnionToIntersectionRes = UnionToIntersection<{ a: 1 } | { b: 2 }>;
+
+// GetOptional
+type GetOptional<Obj extends Record<string, any>> = {
+    [Key in keyof Obj as {} extends Pick<Obj, Key> ? Key : never]: Obj[Key];
+};
+type GetOptionalRes = GetOptional<{ a: 1; b?: 2; c?: 3 }>;
+
+// GetRequired
+type GetRequired<Obj extends Record<string, any>> = {
+    [Key in keyof Obj as {} extends Pick<Obj, Key> ? never : Key]: Obj[Key];
+};
+type GetRequiredRes = GetRequired<{ a: 1; b?: 2; c?: 3 }>;
+
+// RemoveIndexSignature
+type RemoveIndexSignature<Obj extends Record<string, any>> = {
+    [Key in keyof Obj as Key extends `${infer Str}` ? Str : never]: Obj[Key];
+};
+type RemoveIndexSignatureRes = RemoveIndexSignature<{
+    a: 1;
+    [key: string]: number;
+}>;
+
+// ClassPublicProps
+type ClassPublicProps<Obj extends Record<string, any>> = {
+    [Key in keyof Obj]: Obj[Key];
+};
+class Person {
+    public name: string;
+    protected age: number;
+
+    constructor() {
+        this.name = 'puqinsi';
+        this.age = 18;
+    }
+}
+type ClassPublicPropsRes = ClassPublicProps<Person>;
+
+// as const
+const obj = { a: 1, b: 2 };
+const arr = [1, 2, 3];
+type objType = typeof obj;
+type arrType = typeof arr;
+
+const obj2 = { a: 1, b: 2 } as const;
+const arr2 = [1, 2, 3] as const;
+type objType2 = typeof obj2;
+type arrType2 = typeof arr2;
